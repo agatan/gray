b06fc8cfbba2b9b2844ddd64b9ba9b2a2c03cbeb
agatan <agatan039@gmail.com>
2016-11-04 17:09:27 +0900

    Add ExprStmt check

--- types/stmt.go
+++ types/stmt.go
@@ -1,7 +1,26 @@
 package types
 
-import "github.com/agatan/gray/ast"
+import (
+	"fmt"
+
+	"github.com/agatan/gray/ast"
+)
 
 func (c *Checker) checkStmt(s *Scope, stmt ast.Stmt) error {
-	return nil
+	switch stmt := stmt.(type) {
+	case *ast.ExprStmt:
+		ty, err := c.checkExpr(s, stmt.X)
+		if err != nil {
+			return nil
+		}
+		if !c.isSameType(BasicTypes[Unit], ty) {
+			return &Error{
+				Message: fmt.Sprintf("type mismatch: expected %s, but got %s", BasicTypes[Unit], ty),
+				Pos:     stmt.Position(),
+			}
+		}
+		return nil
+	default:
+		panic("unimplemented yet")
+	}
 }
