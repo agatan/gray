aa8d3f7fd67b48ec752bea844d3b2163a3aaf07d
agatan <agatan039@gmail.com>
2016-11-04 15:21:07 +0900

    Add Ident check

--- types/expr.go
+++ types/expr.go
@@ -1,9 +1,22 @@
 package types
 
-import "github.com/agatan/gray/ast"
+import (
+	"fmt"
+
+	"github.com/agatan/gray/ast"
+)
 
 func (c *Checker) checkExpr(s *Scope, e ast.Expr) (Type, error) {
 	switch e := e.(type) {
+	case *ast.Ident:
+		obj := s.LookupParent(e.Name)
+		if obj == nil {
+			return nil, &Error{
+				Message: fmt.Sprintf("undefined variable %s", e.Name),
+				Pos:     e.Position(),
+			}
+		}
+		return obj.Type(), nil
 	case *ast.BlockExpr:
 		if len(e.Stmts) == 0 {
 			return BuiltinTypes[Unit], nil
