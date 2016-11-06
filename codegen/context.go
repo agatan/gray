6c01e56abf21a96dbe30cbcd533af379cb586c9f
agatan <agatan039@gmail.com>
2016-11-06 11:34:17 +0900

    Add llvm target informations

--- codegen/context.go
+++ codegen/context.go
@@ -4,14 +4,29 @@ import "llvm.org/llvm/bindings/go/llvm"
 
 // Context represent a context of code generation.
 type Context struct {
-	llcontext llvm.Context
-	llbuilder llvm.Builder
+	llcontext       llvm.Context
+	llbuilder       llvm.Builder
+	lltarget        llvm.Target
+	lltargetMachine llvm.TargetMachine
+	lltargetData    llvm.TargetData
 }
 
 // NewContext returns a new Context instance.
-func NewContext() *Context {
-	return &Context{
-		llcontext: llvm.GlobalContext(),
-		llbuilder: llvm.NewBuilder(),
+func NewContext() (*Context, error) {
+	if err := llvm.InitializeNativeTarget(); err != nil {
+		return nil, err
 	}
+	triple := llvm.DefaultTargetTriple()
+	target, err := llvm.GetTargetFromTriple(triple)
+	if err != nil {
+		return nil, err
+	}
+	tm := target.CreateTargetMachine(triple, "", "", llvm.CodeGenLevelNone, llvm.RelocDefault, llvm.CodeModelDefault)
+	return &Context{
+		llcontext:       llvm.GlobalContext(),
+		llbuilder:       llvm.NewBuilder(),
+		lltarget:        target,
+		lltargetMachine: tm,
+		lltargetData:    tm.CreateTargetData(),
+	}, nil
 }
