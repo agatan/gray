07af1cd31d4a871b2d94d6af74a8c245046e817e
agatan <agatan039@gmail.com>
2016-11-06 22:14:35 +0900

    While statement

--- codegen/stmt.go
+++ codegen/stmt.go
@@ -3,6 +3,8 @@ package codegen
 import (
 	"fmt"
 
+	"llvm.org/llvm/bindings/go/llvm"
+
 	"github.com/agatan/gray/ast"
 )
 
@@ -19,6 +21,23 @@ func (c *Context) genStmt(s ast.Stmt) {
 		} else {
 			c.llbuilder.CreateRet(c.genExpr(s.X))
 		}
+	case *ast.WhileStmt:
+		parentb := c.llbuilder.GetInsertBlock().Parent()
+		condb := llvm.AddBasicBlock(parentb, "cond")
+		bodyb := llvm.AddBasicBlock(parentb, "body")
+		exitb := llvm.AddBasicBlock(parentb, "exit")
+		// generate cond block
+		c.llbuilder.CreateBr(condb)
+		c.llbuilder.SetInsertPointAtEnd(condb)
+		condv := c.genExpr(s.Cond)
+		cmp := c.llbuilder.CreateICmp(llvm.IntNE, condv, llvm.ConstInt(c.boolType(), 0, false), "whilecond")
+		c.llbuilder.CreateCondBr(cmp, bodyb, exitb)
+		// generate body block
+		c.llbuilder.SetInsertPointAtEnd(bodyb)
+		c.genExpr(s.Body)
+		c.llbuilder.CreateBr(condb)
+		// generate exit block
+		c.llbuilder.SetInsertPointAtEnd(exitb)
 	default:
 		panic(fmt.Sprintf("unimplemented yet: %T", s))
 	}
