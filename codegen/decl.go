4f04f3a1b0a0795034e9a0682028b9ced1529f98
agatan <agatan039@gmail.com>
2016-11-06 17:32:17 +0900

    Add parameter support

--- codegen/decl.go
+++ codegen/decl.go
@@ -11,6 +11,12 @@ func (c *Context) genDecl(d ast.Decl) error {
 	switch d := d.(type) {
 	case *ast.FuncDecl:
 		f := c.llmodule.NamedFunction(c.mangle(d.Ident.Name))
+		c.enterNewScope()
+		defer c.exitScope()
+		// define parameters
+		for i, param := range f.Params() {
+			c.valuemap.Insert(d.Type.Params[i].Ident.Name, param)
+		}
 		bb := llvm.AddBasicBlock(f, "entry")
 		c.llbuilder.SetInsertPointAtEnd(bb)
 		v, err := c.genExpr(d.Body)
