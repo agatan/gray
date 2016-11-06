654c355a9bd9bb415a3fab8c9f7843cbb30b591b
agatan <agatan039@gmail.com>
2016-11-06 21:59:41 +0900

    Return statement

--- codegen/stmt.go
+++ codegen/stmt.go
@@ -8,11 +8,17 @@ import (
 
 func (c *Context) genStmt(s ast.Stmt) {
 	switch s := s.(type) {
+	case *ast.ExprStmt:
+		c.genExpr(s.X)
 	case *ast.LetStmt:
 		v := c.genExpr(s.Value)
 		c.valuemap.Insert(s.Ident.Name, v)
-	case *ast.ExprStmt:
-		c.genExpr(s.X)
+	case *ast.ReturnStmt:
+		if s.X == nil {
+			c.llbuilder.CreateRetVoid()
+		} else {
+			c.llbuilder.CreateRet(c.genExpr(s.X))
+		}
 	default:
 		panic(fmt.Sprintf("unimplemented yet: %T", s))
 	}
